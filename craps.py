8b22719eb503683d6ccdf8d981ee7526e6dee6e3
ariel leventhal <arielleventhal@gmail.com>
2020-03-30 20:11:07 -0300

    adicionado logica de escolher apostas

--- craps.py
+++ craps.py
@@ -1,3 +1,5 @@
+import random
+import time
 #Funcoes de aposta
 def pass_line(dados,aposta):
     if dados in {7,11}:
@@ -34,12 +36,38 @@ def quer_continuar(fichas,fase):
             return False
         print("resposta invalida")
 
+
+    
 continua_jogando = True
 fichas = 100
 fases = ["come out","point"]
 fase = 0
-apostas = [pass_line,field,any_craps,twelve]
+apostas = {"pass line":pass_line,"field":field,"any craps":any_craps,"twelve":twelve}
 apostas_disponiveis = []
+funcoes = globals().copy()
+funcoes.update(locals())
 
 while continua_jogando:
     continua_jogando = quer_continuar(fichas,fases[fase])
+    dados = random.randint(1,6) + random.randint(1,6)
+
+    continuar = True
+    apostas_escolhidas = []
+    apostas_disponiveis = list(apostas.keys())
+    while continuar:
+        print("você tem {} fichase fez {} apostas. Quer adicionar uma aposta?".format(fichas,len(apostas_escolhidas)))
+        for i,item in enumerate(apostas_disponiveis):
+            print("{}. {}".format(i+1,apostas_disponiveis[i]))
+        try:
+            resposta = int(input("{}. finalizar apostas \n".format(len(apostas_disponiveis)+1)))
+        except:
+            resposta = 6
+        if not resposta in range(1,len(apostas_disponiveis)+2):
+            print("resposta invalida\n \n")
+            time.sleep(1)
+        elif resposta == len(apostas_disponiveis)+1:
+            continuar = False
+        else:
+            apostas_escolhidas.append(apostas[apostas_disponiveis[resposta-1]])
+            del apostas_disponiveis[resposta-1]
+            print(apostas_escolhidas)

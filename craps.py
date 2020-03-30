1f1a94acf895399393e670226597c469359795f3
ariel leventhal <arielleventhal@gmail.com>
2020-03-30 19:25:07 -0300

    adicionadas variaveis privipais e loop de jogo

--- craps.py
+++ craps.py
@@ -24,3 +24,22 @@ def twelve(dados,aposta):
     if dados == 12:
         return ("ganhou", 31*aposta)
     return ("perdeu", 0)
+
+def quer_continuar(fichas,fase):
+    while True:
+        jogar = input("você tem {} fichas e esta na fase {}. Quer conttinuar jogando?(s/n)".format(fichas,fase))
+        if jogar == "s":
+            return True
+        elif jogar == "n":
+            return False
+        print("resposta invalida")
+
+continua_jogando = True
+fichas = 100
+fases = ["come out","point"]
+fase = 0
+apostas = [pass_line,field,any_craps,twelve]
+apostas_disponiveis = []
+
+while continua_jogando:
+    continua_jogando = quer_continuar(fichas,fases[fase])
